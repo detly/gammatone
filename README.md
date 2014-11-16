35314bddb04d163740e274b63fa1ec4b4172bada
Jason Heeris <jason.heeris@gmail.com>
2014-11-16 15:15:22 +1100

    Squashed commit of the following:
    commit 75943e0f355c31cc12a78d1217422d815063f1d1
    Author: Jason Heeris <jason.heeris@gmail.com>
    Date:   Sun Nov 16 15:02:22 2014 +1100
    
        Fixed link in code docs.
    
    commit 3cdf24dd5a60f02924d66324365e36dbc9eef32c
    Author: Jason Heeris <jason.heeris@gmail.com>
    Date:   Sun Nov 16 14:56:03 2014 +1100
    
        Updated documentation now that audiolab has been removed.
    
    commit 17ab327433b6c0ee8ba3ad815ebbbdc472287c00
    Author: Jason Heeris <jason.heeris@gmail.com>
    Date:   Sat Sep 13 23:27:37 2014 +1000
    
        Plotting script only accepts WAV now.
    
    commit 853d7a58f46c03edaf50ea739b3e88d834550d75
    Author: Jason Heeris <jason.heeris@gmail.com>
    Date:   Sat Sep 13 18:41:00 2014 +1000
    
        Removed scikits.audiolab from dependencies.

--- README.md
+++ README.md
@@ -45,27 +45,23 @@ python setup.py install
  - nose
  - mock
  - matplotlib
- - scikits.audiolab >= 0.11.0.p3.1
-
-Because the authoritative version of `audiolab` hasn't been ported to Python 3
-yet, you will need to install [my port](https://github.com/detly/audiolab).
 
 Using the Code
 --------------
 
 See the [API documentation](http://detly.github.io/gammatone/). For a
-demonstration, you can run:
+demonstration, find a `.wav` file (for example,
+[Für Elise](http://heeris.id.au/samples/FurElise.wav)) and run:
 
 ```text
-python -m gammatone samples/FurElise.ogg -d 10
+python -m gammatone FurElise.wav -d 10
 ```
 
-...to see a gammatone-gram of the first ten seconds of Beethoven's "Für Elise."
-If you've installed via `pip` or `setup.py install`, you should also be able to
-just run:
+...to see a gammatone-gram of the first ten seconds of the track. If you've
+installed via `pip` or `setup.py install`, you should also be able to just run:
 
 ```text
-gammatone samples/FurElise.ogg -d 10
+gammatone FurElise.wav -d 10
 ```
 
 Basis
@@ -137,7 +133,7 @@ workflow:
 
   1. Run the scripts in the `test_generation` directory. This will create a
      `.mat` file containing test data in `tests/data`.
-     
+
   2. Run `nosetest3` in the top level directory. This will find and run all the
      tests in the `tests` directory.
 
--- gammatone/plot.py
+++ gammatone/plot.py
@@ -1,5 +1,5 @@
 # Copyright 2014 Jason Heeris, jason.heeris@gmail.com
-# 
+#
 # This file is part of the gammatone toolkit, and is licensed under the 3-clause
 # BSD license: https://github.com/detly/gammatone/blob/master/COPYING
 """
@@ -9,11 +9,11 @@ Plotting utilities related to gammatone analysis, primarily for use with
 import argparse
 import os.path
 
-import audiolab
 import matplotlib.pyplot
 import matplotlib.ticker
 import numpy as np
 import scipy.constants
+import scipy.io.wavfile
 
 from .filters import erb_point
 import gammatone.gtgram
@@ -31,13 +31,13 @@ class ERBFormatter(matplotlib.ticker.EngFormatter):
     with ``imshow`` where the ``extent`` argument is ``[a, b, 1, 0]`` (again,
     note the inversion).
     """
-    
+
     def __init__(self, low_freq, high_freq, *args, **kwargs):
         """
         Creates a new :class ERBFormatter: for use with ``matplotlib`` plots.
         Note that this class does not supply the ``units`` or ``places``
         arguments; typically these would be ``'Hz'`` and ``0``.
-        
+
         :param low_freq: the low end of the gammatone filterbank frequency range
         :param high_freq: the high end of the gammatone filterbank frequency
           range
@@ -52,7 +52,7 @@ class ERBFormatter(matplotlib.ticker.EngFormatter):
     def __call__(self, val, pos=None):
         newval = self._erb_axis_scale(val)
         return super().__call__(newval, pos)
-    
+
 
 def gtgram_plot(
         gtgram_function,
@@ -78,41 +78,27 @@ def gtgram_plot(
     # Set a nice formatter for the y-axis
     formatter = ERBFormatter(f_min, fs/2, unit='Hz', places=0)
     axes.yaxis.set_major_formatter(formatter)
-    
+
     # Figure out time axis scaling
     duration = len(x) / fs
-    
+
     # Calculate 1:1 aspect ratio
     aspect_ratio = duration/scipy.constants.golden
-    
+
     gtg = gtgram_function(x, fs, window_time, hop_time, channels, f_min)
     Z = np.flipud(20 * np.log10(gtg))
-    
+
     img = axes.imshow(Z, extent=[0, duration, 1, 0], aspect=aspect_ratio)
 
 
 # Entry point for CLI script
 
 HELP_TEXT = """\
-Plots the gammatone filterbank analysis of a sound file. The sound file can be
-anything supported by audiolab, which on this machine is:
-{sound_formats}
+Plots the gammatone filterbank analysis of a WAV file.
 
 If the file contains more than one channel, all channels are averaged before
 performing analysis.
 """
-
-FORMAT_LIST_PREFIX = "  - "
-
-def _format_help_text(help_text):
-    """
-    Inserts the list of supported audio formats (obtained from audiolab on the
-    current machine) into the help text show for command line invocation.
-    """
-    formats = audiolab.available_file_formats()
-    format_entries = (FORMAT_LIST_PREFIX + fmt for fmt in formats)
-    sound_format_text = ", ".join(format_entries)
-    return help_text.format(sound_formats=sound_format_text)
 
 
 def render_audio_from_file(path, duration, function):
@@ -120,14 +106,14 @@ def render_audio_from_file(path, duration, function):
     Renders the given ``duration`` of audio from the audio file at ``path``
     using the gammatone spectrogram function ``function``.
     """
-    music = audiolab.Sndfile(path)
-    # Average the stereo signal
-    if not duration:
-        nframes = music.nframes
-    else:
-        nframes = duration * music.samplerate
+    samplerate, data = scipy.io.wavfile.read(path)
 
-    signal = music.read_frames(nframes).mean(1)
+    # Average the stereo signal
+    if duration:
+        nframes = duration * samplerate
+        data = data[0:nframes, :]
+
+    signal = data.mean(1)
 
     # Default gammatone-based spectrogram parameters
     twin = 0.08
@@ -143,7 +129,7 @@ def render_audio_from_file(path, duration, function):
         function,
         axes,
         signal,
-        music.samplerate,
+        samplerate,
         twin, thop, channels, fmin)
 
     axes.set_title(os.path.basename(path))
@@ -151,13 +137,13 @@ def render_audio_from_file(path, duration, function):
     axes.set_ylabel("Frequency")
 
     matplotlib.pyplot.show()
-    
+
 
 def main():
     """
     Entry point for CLI application to plot gammatonegrams of sound files.
     """
-    parser = argparse.ArgumentParser(description=_format_help_text(HELP_TEXT))
+    parser = argparse.ArgumentParser(description=HELP_TEXT)
 
     parser.add_argument(
         'sound_file',
